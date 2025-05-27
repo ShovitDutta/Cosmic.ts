884def1a18c266ae94b32c24f8300789dcf67c56
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-27 21:45:27 +0530

    initial monorepo.ts stage

---
+++ config/tailwind-config/postcss.config.js
@@ -0,0 +1,1 @@
+export const postcssConfig = { plugins: { "@tailwindcss/postcss": {} } };
---
+++ config/tailwindcss/eslint.config.mjs
@@ -0,0 +1,3 @@
+import { config } from "@repo/eslint-config/react-internal";
+/** @type {import("eslint").Linter.Config} */
+export default config;
