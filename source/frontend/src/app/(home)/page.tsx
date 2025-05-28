553c3fb05915f01dc7e080965fbfec79eba0e1d1
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-28 08:17:32 +0530

    chore(home): update import paths for SignIn and Dashboard components

--- source/frontend/src/app/(home)/page.tsx
+++ source/frontend/src/app/(home)/page.tsx
@@ -1,8 +1,8 @@
 // source/frontend/src/app/(home)/page.tsx
 import React from "react";
 import { auth } from "../../../auth";
-import SignIn from "../../components/client/(root-page)/signin";
-import { Dashboard } from "../../components/server/(root-page)/Dashboard";
+import SignIn from "./_components/client/signin";
+import { Dashboard } from "./_components/server/Dashboard";
 export default async function Page() {
     const session = await auth();
     if (!session) return <SignIn />;
