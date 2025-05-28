09b6bf07ddc5da1388cbbbdbf4a36ea5e17d6c1c
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-28 07:45:36 +0530

    feat(home-page): Add Dashboard component for authenticated users

--- source/frontend/src/app/(home)/page.tsx
+++ source/frontend/src/app/(home)/page.tsx
@@ -1,30 +1,10 @@
 // source/frontend/src/app/(home)/page.tsx
+import React from "react";
 import { auth } from "../../../auth";
-import React, { Fragment } from "react";
 import SignIn from "../../components/client/(root-page)/signin";
-import { SignOut } from "../../components/client/(root-page)/signout";
-import { UserAvatar } from "../../components/server/(root-page)/UserAvatar";
+import { Dashboard } from "../../components/server/(root-page)/Dashboard";
 export default async function Page() {
     const session = await auth();
     if (!session) return <SignIn />;
-    return (
-        <Fragment>
-            <UserAvatar session={session} />
-            <ul>
-                <li>
-                    Email:
-                    <span>{session.user?.email}</span>
-                </li>
-                <li>
-                    Id:
-                    <span>{session.user?.id}</span>
-                </li>
-                <li>
-                    Name:
-                    <span>{session.user?.name}</span>
-                </li>
-            </ul>
-            <SignOut />
-        </Fragment>
-    );
+    return <Dashboard session={session} />;
 }
