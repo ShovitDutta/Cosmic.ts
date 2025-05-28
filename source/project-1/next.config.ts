4a62da9b7e73b73a6bcfdc7757fea13becb8d348
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-28 12:52:36 +0530

    MonoRepo.js

--- source/project-1/next.config.ts
+++ source/project-1/next.config.ts
@@ -5,17 +5,19 @@ const nextConfig: NextConfig = {
     typescript: { ignoreBuildErrors: true },
     images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
     async headers() {
-        return [
-            {
-                source: "/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Origin", value: "*" },
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ],
-            },
-        ];
+        if (process.env.NODE_ENV === "development") {
+            return [
+                {
+                    source: "/:path*",
+                    headers: [
+                        { key: "Access-Control-Allow-Origin", value: "*" },
+                        { key: "Access-Control-Allow-Credentials", value: "true" },
+                        { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                        { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                    ],
+                },
+            ];
+        } else return [];
     },
 };
 export default nextConfig;
