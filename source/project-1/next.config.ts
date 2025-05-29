9e9ad640dbaa21f06b0f21938a47a6a013c134e1
Shovit Dutta <shovitdutta1@gmail.com>
2025-05-29 17:46:44 +0530

    asd

--- source/project-1/next.config.ts
+++ source/project-1/next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
     typescript: { ignoreBuildErrors: true },
     images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
     basePath: process.env.NODE_ENV === "production" ? "/project-1" : "",
+    env: { NEXT_PUBLIC_BASE_PATH: "/project-1", NEXT_PUBLIC_INTERNAL_API_HOST: "project-1:3001" },
     async headers() {
         if (process.env.NODE_ENV === "development") {
             return [
