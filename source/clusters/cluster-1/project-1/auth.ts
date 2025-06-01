32d5735ad612ac2a217d6876edcb753caebb7d92
Shovit Dutta <shovitdutta1@gmail.com>
2025-06-01 17:26:44 +0530

    Cosmic.ts

--- source/clusters/cluster-1/project-1/auth.ts
+++ source/clusters/cluster-1/project-1/auth.ts
@@ -1,12 +1,13 @@
+import { db } from "./drizzle";
 import NextAuth from "next-auth";
-import { PrismaClient } from "@prisma/client";
-import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import GitHub from "next-auth/providers/github";
 import Discord from "next-auth/providers/discord";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+if (!db) throw new Error("Database is not initialized");
 export const { handlers, auth, signIn, signOut } = NextAuth({
     providers: [Google, Discord, GitHub],
-    adapter: PrismaAdapter(new PrismaClient()),
+    adapter: DrizzleAdapter(db),
     basePath: process.env.NEXT_PUBLIC_BASE_PATH + "/api/auths",
     pages: { signIn: "/", signOut: "/", error: "/api/auth/error" },
     ...(process.env.NODE_ENV === "production"
--- source/clusters/cluster-1/project-2/src/app/api/peer/route.ts
+++ source/clusters/cluster-1/project-2/src/app/api/peer/route.ts
@@ -1,24 +1,5 @@
 import { NextResponse } from "next/server";
-import { headers } from "next/headers";
-import { basePort, basePath, basePeers } from "../../../utils/dynamicEnv";
-
+import { basePort, basePath } from "../../../utils/dynamicEnv";
 export async function GET() {
-    const origin = (await headers()).get("origin");
-    const allowedOrigins = basePeers ? basePeers.split(",") : [];
-
-    let corsOrigin = '';
-    if (origin && allowedOrigins.includes(origin)) {
-        corsOrigin = origin;
-    }
-
-    return NextResponse.json(
-        { basePort, basePath },
-        {
-            headers: {
-                "Access-Control-Allow-Origin": corsOrigin,
-                "Access-Control-Allow-Headers": "Content-Type, Authorization",
-                "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-            },
-        },
-    );
+    return NextResponse.json({ basePort, basePath });
 }
