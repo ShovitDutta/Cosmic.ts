30b0c29067b69177997320e0d4ce6934ea2ffe70
Shovit Dutta <shovitdutta1@gmail.com>
2025-06-03 21:56:15 +0530

    ☻ cosmos.ts ☻

--- source/__main__/tsup.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "tsup";
-export default defineConfig({
-    entry: ["scripts/launch.ts"],
-    outDir: "scripts",
-    splitting: false,
-    format: ["cjs"],
-    clean: false,
-});
